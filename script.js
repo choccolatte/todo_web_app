c30b58b75304015575b5585a927fcdcb645c627c
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-04-06 23:13:48 +0530

    complete script.js

--- script.js
+++ script.js
@@ -24,11 +24,10 @@ const addOrUpdateTask = () => {
   }
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
   const taskObj = {
-    id: `${(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
-    title: titleInput.value,
+    id: `${removeSpecialChars(titleInput.value).toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: removeSpecialChars(titleInput.value),
     date: dateInput.value,
-    description: descriptionInput.value,
-    removeSpecialChars()
+    description: removeSpecialChars(descriptionInput.value),
   };
 
   if (dataArrIndex === -1) {
